2d826b50e4514c7dfe3d9034f5e01ed82b1bce02
Ajith <ajithunair@outlook.com>
2022-07-01 11:37:03 +0530

    Added AuthenticationController for generating JWT

---
+++ Controllers/AuthenticationController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using WebApiJwtAuth.Models;
+
+namespace WebApiJwtAuth.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] Login user)
+        {
+            if (user == null)
+                return BadRequest("Invalid user request");
+            if (user.UserName == "Ajith" && user.Password == "1234")
+            {
+                var secret = ConfigurationManager.Configuration["Jwt:Secret"];
+                var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secret));
+                var signingCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+                var securityToken = new JwtSecurityToken
+                (
+                    issuer : ConfigurationManager.Configuration["Jwt:Issuer"],
+                    audience: ConfigurationManager.Configuration["Jwt:Audience"],
+                    signingCredentials: signingCredentials,
+                    claims: new List<Claim>(),
+                    expires: DateTime.Now.AddMinutes(5)
+                );
+                var tokenString = new JwtSecurityTokenHandler().WriteToken(securityToken);
+                return Ok(new JwtTokenResponse { Token = tokenString });
+            }
+
+            return Unauthorized();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
 using WebApiJwtAuth.Data;
 using ConfigurationManager = WebApiJwtAuth.ConfigurationManager;
 
@@ -10,7 +14,56 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddDbContext<DbContextClass>(options => options.UseSqlServer(ConfigurationManager.Configuration.GetConnectionString("ProductsDB")));
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("V1", new OpenApiInfo
+    {
+        Version = "V1",
+        Title = "WebApi with JWT Auth",
+        Description = "WebApi with JWT Auth"
+    });
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Scheme = JwtBearerDefaults.AuthenticationScheme,
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Name = "Authorization",
+        Description = "WebApi with JWT Bearer Authentication",
+        Type = SecuritySchemeType.Http
+    });
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+        new OpenApiSecurityScheme
+        {
+            Reference = new OpenApiReference
+            {
+                Id="Bearer",
+                Type = ReferenceType.SecurityScheme
+            }
+        },
+            new List<string>()
+        }
+    });
+});
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = ConfigurationManager.Configuration["Jwt:Issuer"],
+        ValidAudience = ConfigurationManager.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(ConfigurationManager.Configuration["Jwt:Secret"]))
+    };
+});
 
 var app = builder.Build();
 
@@ -18,7 +71,9 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options => {
+        options.SwaggerEndpoint("/swagger/V1/swagger.json", "Product WebAPI");
+    });
 }
 
 app.UseHttpsRedirection();
